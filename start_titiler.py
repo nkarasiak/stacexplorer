78a39ccfbab903c9ad28b50cb12ebbd0aabdde7e
Nicolas Karasiak <nicart@gmail.com>
2025-03-05 10:50:19 +0100

    feat: maplibre instead of leaflet

--- start_titiler.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-"""
-Simple script to start a TiTiler server
-"""
-import uvicorn
-from titiler.application import app
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000) 
